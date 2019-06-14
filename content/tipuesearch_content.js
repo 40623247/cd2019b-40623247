f4d9abed879cecd35fb0d569fbe659622d9d0953
40623247 <40623247@gm.nfu.edu.tw>
2019-06-14 13:54:33 +0800

    add link

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': '簡介', 'text': '', 'tags': '', 'url': '簡介.html'}, {'title': '手足球系統模擬', 'text': '\n \n', 'tags': '', 'url': '手足球系統模擬.html'}, {'title': '期末報告影片', 'text': '\n', 'tags': '', 'url': '期末報告影片.html'}]};
+var tipuesearch = {"pages": [{'title': '簡介', 'text': '個人倉儲: https://github.com/40623247/cd2019b-40623247', 'tags': '', 'url': '簡介.html'}, {'title': '手足球系統模擬', 'text': '\n \n', 'tags': '', 'url': '手足球系統模擬.html'}, {'title': '期末報告影片', 'text': '\n', 'tags': '', 'url': '期末報告影片.html'}]};
